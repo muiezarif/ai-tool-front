737fedb53fbacc0e78d02e398a21c86e3f311b19
muiezarif <36100308+muiezarif@users.noreply.github.com>
2023-01-29 18:27:35 +0500

    next config change cors added

--- api/baseApi.js
+++ api/baseApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"https://ai-tool-lyart.vercel.app"
+    baseURL:"https://ai-tool-lyart.vercel.app/api"
     // baseURL:"https://digitvl.live/api/v1"
     // baseURL:"https://96db-119-63-138-150.ngrok.io/api/v1"
 });
